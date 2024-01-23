39573e7f530dd005164a384ef57cb60c206d09da
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-01-23 09:13:55 +0900

    Start

---
+++ Init_Windows/Init_Direct3D/D3DApp.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#if defined(DEBUG) || defined(_DEBUG)
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+#endif
+
+#include "../Common/d3dUtil.h"
+#include "../Common/GameTimer.h"
+
+// Link necessary d3d12 libraries.
+#pragma comment(lib,"d3dcompiler.lib")
+#pragma comment(lib, "D3D12.lib")
+#pragma comment(lib, "dxgi.lib")
+
+class D3DApp
+{
+protected:
+    D3DApp(HINSTANCE hInstance);
+    D3DApp(const D3DApp& rhs) = delete;
+    D3DApp& operator=(const D3DApp& rhs) = delete;
+    virtual ~D3DApp();
+
+public:
+    static D3DApp* GetApp();
+
+    HINSTANCE AppInst()const;
+    HWND      MainWnd()const;
+    float     AspectRatio()const;
+
+    int Run();
+
+    virtual bool Initialize();
+    virtual LRESULT MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+protected:
+    virtual void OnResize();
+    virtual void Update(const GameTimer& gt) = 0;
+    virtual void Draw(const GameTimer& gt) = 0;
+
+    // Convenience overrides for handling mouse input.
+    virtual void OnMouseDown(WPARAM btnState, int x, int y) { }
+    virtual void OnMouseUp(WPARAM btnState, int x, int y) { }
+    virtual void OnMouseMove(WPARAM btnState, int x, int y) { }
+
+protected:
+    bool InitMainWindow();
+
+    void CalculateFrameStats();
+
+protected:
+
+    static D3DApp* mApp;
+
+    HINSTANCE mhAppInst = nullptr; // application instance handle
+    HWND      mhMainWnd = nullptr; // main window handle
+    bool      mAppPaused = false;  // is the application paused?
+    bool      mMinimized = false;  // is the application minimized?
+    bool      mMaximized = false;  // is the application maximized?
+    bool      mResizing = false;   // are the resize bars being dragged?
+    bool      mFullscreenState = false;// fullscreen enabled
+
+    std::wstring mMainWndCaption = L"d3d App";
+
+    int mClientWidth = 800;
+    int mClientHeight = 600;
+
+    // Used to keep track of the “delta-time?and game time (?.4).
+    GameTimer mTimer;
+};
+
---
+++ Init_Windows/Init_Direct3D/InitDirect3DApp.cpp
@@ -0,0 +1,56 @@
+#include "InitDirect3DApp.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
+    PSTR cmdLine, int showCmd)
+{
+    // Enable run-time memory check for debug builds.
+#if defined(DEBUG) | defined(_DEBUG)
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+    try
+    {
+        InitDirect3DApp theApp(hInstance);
+        if (!theApp.Initialize())
+            return 0;
+
+        return theApp.Run();
+    }
+    catch (DxException& e)
+    {
+        MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
+        return 0;
+    }
+}
+
+InitDirect3DApp::InitDirect3DApp(HINSTANCE hInstance)
+    : D3DApp(hInstance)
+{
+}
+
+InitDirect3DApp::~InitDirect3DApp()
+{
+}
+
+bool InitDirect3DApp::Initialize()
+{
+    if (!D3DApp::Initialize())
+        return false;
+
+    return true;
+}
+
+void InitDirect3DApp::OnResize()
+{
+    D3DApp::OnResize();
+}
+
+void InitDirect3DApp::Update(const GameTimer& gt)
+{
+
+}
+
+void InitDirect3DApp::Draw(const GameTimer& gt)
+{
+
+}
